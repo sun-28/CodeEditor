05951515748e35ce3adc8d91359529c83e5ca522
Sun28 <sunpahwa28@gmail.com>
2023-10-26 20:48:22 +0530

    final editor

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Landing from "./components/Landing";
+import Navbar from "./components/Navbar";
+
+function App() {
+  
+  return (
+    <>
+    <Navbar/>
+    <Landing/>
+    </>
+  );
+}
+
+export default App;
